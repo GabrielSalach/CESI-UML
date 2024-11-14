a68ef58edaa1a47f26d62af2db9fe75ba2f68b66
Gabriel <gabsalach@orange.fr>
2024-11-14 16:32:35 +0100

    xyz

---
+++ xyz/src/models/User.ts
@@ -0,0 +1,12 @@
+class User {
+    id : string;
+    username : string;
+    email : string;
+    password : string;
+    createdAt : Date;
+    lastSignInAt : Date;
+
+    constructor() {
+        this.id = "0";
+    }
+}
